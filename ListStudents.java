568cdf0bda9a4d67dd37c83afff81190785ac0e2
OOP_Xander <143883878+xand069@users.noreply.github.com>
2023-11-04 12:14:55 +0800

    Add files via upload

--- ListStudents.java
+++ ListStudents.java
@@ -15,9 +15,11 @@ public class ListStudents {
             System.out.println("1. Add a student");
             System.out.println("2. Edit a student");
             System.out.println("3. View the list of students");
-            System.out.println("4. Exit");
+            System.out.println("4. Delete a student");
+            System.out.println("5. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
 
             switch (choice) {
                 case 1:
@@ -38,62 +40,58 @@ public class ListStudents {
                     viewStudents(students);
                     break;
                 case 4:
+                    deleteStudent(students, scanner);
+                    break;
+                case 5:
                     System.out.println("Exiting the program.");
                     break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
-        } while (choice != 4);
+        } while (choice != 5);
 
         scanner.close();
     }
 
-public static Student addStudent(Scanner scanner) {
-    System.out.print("Do you want to add a new student (Y/N)? ");
-    String addNewStudent = scanner.next();
-    scanner.nextLine(); // Consume the newline character
+    public static Student addStudent(Scanner scanner) {
+        System.out.print("Enter the student's name or press Enter to skip: ");
+        String name = scanner.nextLine();
 
-    if (addNewStudent.equalsIgnoreCase("N")) {
-        // User chose not to add a new student
-        return null;
+        System.out.print("Enter the student's course or press Enter to skip: ");
+        String course = scanner.nextLine();
+
+        System.out.print("Enter the student's GWA or press Enter to skip: ");
+        String gwaInput = scanner.nextLine();
+
+        String defaultName = "N/A";
+        String defaultCourse = "N/A";
+        double defaultGWA = -1.0;
+
+        if (name.isEmpty()) {
+            name = defaultName;
+        }
+
+        if (course.isEmpty()) {
+            course = defaultCourse;
+        }
+
+        if (gwaInput.isEmpty()) {
+            return new Student(name, course, defaultGWA);
+        } else {
+            try {
+                double gwa = Double.parseDouble(gwaInput);
+                return new Student(name, course, gwa);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. The student will be recorded with default values.");
+                return new Student(name, course, defaultGWA);
+            }
+        }
     }
-
-    System.out.print("Enter the student's name or press Enter to skip: ");
-    String name = scanner.nextLine().trim();
-    
-    if (name.isEmpty()) {
-        name = "N/A";
-    }
-
-    System.out.print("Enter the student's course or press Enter to skip: ");
-    String course = scanner.nextLine().trim();
-    
-    if (course.isEmpty()) {
-        course = "N/A";
-    }
-
-    System.out.print("Enter the student's GWA or press Enter to skip: ");
-    String gwaInput = scanner.nextLine().trim();
-    
-    if (gwaInput.isEmpty()) {
-        return new Student(name, course, -1.0); // Use a placeholder value for skipped GWA.
-    }
-
-    double gwa;
-
-    try {
-        gwa = Double.parseDouble(gwaInput);
-    } catch (NumberFormatException e) {
-        System.out.println("Invalid input. Please enter a valid GWA or press Enter to skip.");
-        return addStudent(scanner); // Recursively call addStudent if input is invalid.
-    }
-
-    return new Student(name, course, gwa);
-}
 
     public static void editStudent(Student[] students, Scanner scanner) {
         System.out.print("Enter the index of the student you want to edit (1-" + students.length + "): ");
         int index = scanner.nextInt();
+        scanner.nextLine();
 
         if (index >= 1 && index <= students.length) {
             Student editedStudent = addStudent(scanner);
@@ -114,6 +112,23 @@ public static Student addStudent(Scanner scanner) {
             if (students[i] != null) {
                 System.out.println((i + 1) + ". " + students[i]);
             }
+        }
+    }
+
+    public static void deleteStudent(Student[] students, Scanner scanner) {
+        System.out.print("Enter the index of the student you want to delete (1-" + students.length + "): ");
+        int index = scanner.nextInt();
+        scanner.nextLine();
+
+        if (index >= 1 && index <= students.length) {
+            if (students[index - 1] != null) {
+                students[index - 1] = null;
+                System.out.println("Student at index " + index + " has been deleted.");
+            } else {
+                System.out.println("No student exists at index " + index + ".");
+            }
+        } else {
+            System.out.println("Invalid index. Please enter a number between 1 and " + students.length + " for an existing student.");
         }
     }
 }
